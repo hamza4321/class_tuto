b60f36ca1ee8d63fdb6842abffea115af8bdc063
hamza <hsehnoun@hotmail.com>
2018-01-18 13:33:27 +0100

    app.rb

--- voiture.rb
+++ voiture.rb
@@ -1,18 +1,30 @@
 class Voiture 
 
+
+	def initialize(couleur)
+		@couleur = couleur
+		puts "je suis en train de m'initialiser"
+	end
+
 	def klaxonner
 		puts "PouetPouet"
 	end
 
 	def tourner(direction)
 		if direction == "droite" || direction == "d" 
-			puts "je tourne à droite"
+			puts "je tourne à droite" #étant donné que la direction est indiquée par les conditions présentées dans le "if"
+ 
+
 		elsif direction == "gauche" || direction == "g"
-			puts "je tourne à gauche"
-			
+			puts "je tourne à gauche" 
 		end
 	
 	end
+
+	def afficher_ma_couleur()
+		puts "j'ai une voiture de couleur #{@couleur} !"
+	end
+
 end
 
 
